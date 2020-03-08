b06a26738c4e3a89cf1361c47d8e1b9a913ef359
EloYoshida <helolondrina@gmail.com>
2020-03-08 18:51:10 -0300

    gets e setters

--- aluno.java
+++ aluno.java
@@ -1,8 +1,20 @@
-public class Aluno{
-   private String nome;
-   private int id;   
-}
+package br.usjt;
 
-public void getNome()[
-    return nome;
-]
+public class Aluno {
+
+	private String nome;
+	private int id;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getId() {
+		return id;
+	}
+
+}
